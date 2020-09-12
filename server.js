58f1a5952b7011754f5b39b4ca5003e18b50e0bd
Osman <osmannamso123@gmail.com>
2020-09-12 15:02:42 +0600

    fix: heroku port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(express.static('./dist/frootkz'));
 
@@ -8,4 +9,6 @@ app.get('/*', function(req, res) {
 );
 });
 
-app.listen(8080);
+app.listen(port, () => {
+  console.log(`Salamaleikum vsem ya na porte: ${port}`);
+});
